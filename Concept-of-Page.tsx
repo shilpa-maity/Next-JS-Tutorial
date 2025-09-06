56e24a81b1c3b9a32049328c92b2897425f0d212
Shilpa Maity <142687908+shilpa-maity@users.noreply.github.com>
2025-09-06 17:30:48 +0530

    Create Concept-of-Page.tsx

---
+++ Concept-of-Page.tsx
@@ -0,0 +1,9 @@
+export default function Home(){
+  return(
+    <div className="grid grid-rows-[20px_1fr_20px] items-center">
+      <h1>This is our homepage at root route/root level</h1>
+    </div>
+    );
+}
+
+//it is root directory which is inside app
